8120a80419495555c882724ee274419b1e95a941
Daniel Bekan <bekandani@gmail.com>
2026-09-16 21:22:34 +0200

    add react compiler and some responsive styling

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,19 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 const rootDir = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(rootDir, 'src'),
